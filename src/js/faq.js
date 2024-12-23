b38976a7be659e6eebfade9cc7e489bd2c894845
Timofey-Byunenko1 <byunenkotimofey@gmail.com>
2024-12-23 13:57:59 +0200

    add index file

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,8 +1,17 @@
-document.querySelectorAll('.question').forEach(button => {
-    button.addEventListener('click', () => {
-      button.classList.toggle('active');
-      const answer = button.nextElementSibling;
-      answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+// Логика для FAQ (показ/скрытие контента)
+document.querySelectorAll('.faq-header').forEach(header => {
+  header.addEventListener('click', () => {
+    const item = header.parentElement;
+
+    // Закрываем другие открытые элементы
+    document.querySelectorAll('.faq-item').forEach(el => {
+      if (el !== item) {
+        el.classList.remove('active'); // Скрываем остальные элементы
+      }
     });
+
+    // Открываем или закрываем текущий элемент
+    item.classList.toggle('active');
   });
+});
   
