82c255a781f1e11b55dd692ca62b01f88b6e000d
Oksana Kolesova <kolesovaok85@gmail.com>
2024-12-25 00:14:26 +0200

    add workTG modalWindow.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,28 @@
 const body = document.querySelector('body');
-const windowModal = document.querySelector('.modal');
+const windowModal = document.querySelector('.modal-worktogether');
 const closeBtn = document.querySelector('.modal-window-btn');
 const OPEN_MODAL = 'is-open';
 
 export const openModal = data => {
   windowModal.classList.add(OPEN_MODAL);
   body.style.overflow = 'hidden';
+  modalRender(data);
 };
+
+function modalRender({ title = '', message = '' }) {
+  const modalTitle = document.createElement('h2');
+  modalTitle.classList.add('modal-title');
+  modalTitle.textContent = title;
+
+  const modalMessage = document.createElement('p');
+  modalMessage.classList.add('modal-message');
+  modalMessage.textContent = message;
+  clearModal();
+
+  const modalWindow = document.querySelector('.modal-window');
+  modalWindow.appendChild(modalTitle);
+  modalWindow.appendChild(modalMessage);
+}
 
 closeBtn.addEventListener('click', () => {
   closeWindow();
@@ -31,8 +47,8 @@ function closeWindow() {
 }
 
 function clearModal() {
-  const modalTitle = document.querySelector('.modal-window-title');
-  const modalMessage = document.querySelector('.modal-window-text');
+  const modalTitle = document.querySelector('.modal-title');
+  const modalMessage = document.querySelector('.modal-message');
   if (modalTitle) {
     modalTitle.remove();
   }
